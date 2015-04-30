09050cd10ce69d8477af90ef7d573fe8ea1718da
Alvaro <alvaro84_yo@hotmail.com>
2015-04-30 17:03:37 +0200

    actualizacion añadir metodo

--- class.java
+++ class.java
@@ -1,2 +1,12 @@
 @author Alvaro Perez Maestro
-private String nombre;
+public class class{
+     public static void main(String[] args){
+                 private String nombre;
+		 int edad;
+		 edad=num();
+     }
+      public int num(){
+           return num;
+      }
+   
+}
